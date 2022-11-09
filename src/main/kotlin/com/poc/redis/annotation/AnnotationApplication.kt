20302ff110260b55f91e5ad725c6c4ae409bff33
Lemoel Marques <lemoel@gmail.com>
2022-11-09 17:18:16 -0300

    Usando redis com as anotações do spring-boot

--- src/main/kotlin/com/poc/redis/annotation/AnnotationApplication.kt
+++ src/main/kotlin/com/poc/redis/annotation/AnnotationApplication.kt
@@ -2,7 +2,9 @@ package com.poc.redis.annotation
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
+@EnableCaching
 @SpringBootApplication
 class AnnotationApplication
 
--- src/main/kotlin/com/poc/redis/annotation/config/ConfigRedis.kt
+++ src/main/kotlin/com/poc/redis/annotation/config/ConfigRedis.kt
@@ -2,7 +2,6 @@ package com.poc.redis.annotation.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
@@ -12,7 +11,6 @@ import org.springframework.data.redis.serializer.RedisSerializationContext
 import java.time.Duration
 
 @Configuration
-@EnableCaching
 class ConfigRedis(
     @Value("\${spring.redis.caches-ttl.student}") private val ttlStudent: Long,
 ) {
